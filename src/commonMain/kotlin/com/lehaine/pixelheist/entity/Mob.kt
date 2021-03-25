51efe75bbaf9ab690979dcbf196fc49e1ad92089
LeHaine <colt.daily@gmail.com>
2021-03-25 15:59:19 -0400

    add initial fsm to mob

--- src/commonMain/kotlin/com/lehaine/pixelheist/entity/Mob.kt
+++ src/commonMain/kotlin/com/lehaine/pixelheist/entity/Mob.kt
@@ -1,7 +1,8 @@
 package com.lehaine.pixelheist.entity
 
-import com.lehaine.lib.registerState
+import com.lehaine.lib.stateMachine
 import com.lehaine.pixelheist.*
+import com.soywiz.klock.TimeSpan
 import com.soywiz.korge.view.ViewDslMarker
 import kotlin.math.abs
 
@@ -17,13 +18,29 @@ class Mob(
     data: World.EntityMob, assets: Assets, level: GameLevel,
 ) : Entity(data.cx, data.cy, assets, level, data.pivotX.toDouble(), data.pivotY.toDouble()) {
 
-    val runSpeed = 0.03
 
-    init {
-        sprite.apply {
-            registerState(assets.mobRun) { abs(dx) >= 0.01 }
-            registerState(assets.mobIdle) { true }
+    private sealed class MobState {
+        object Idle : MobState()
+        object Run : MobState()
+    }
+
+    private val movementFsm = stateMachine<MobState> {
+        state(MobState.Run) {
+            reason { abs(dx) >= 0.01 }
+            begin { sprite.playAnimationLooped(assets.mobRun) }
+        }
+        state(MobState.Idle) {
+            reason { true }
+            begin { sprite.playAnimationLooped(assets.mobIdle) }
+        }
+
+        stateChanged {
+            debugLabel.text = it::class.simpleName ?: ""
         }
     }
 
+    override fun update(dt: TimeSpan) {
+        super.update(dt)
+        movementFsm.update(dt)
+    }
 }
