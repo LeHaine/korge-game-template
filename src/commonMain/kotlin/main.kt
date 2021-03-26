9c60cf515cebbd228770973e2a1d8680c109fdd3
LeHaine <colt.daily@gmail.com>
2021-03-25 22:39:13 -0400

    add Fx placeholder
    update scaleMode to COVER

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -5,6 +5,7 @@ import com.soywiz.korge.Korge
 import com.soywiz.korge.scene.Module
 import com.soywiz.korim.color.Colors
 import com.soywiz.korinject.AsyncInjector
+import com.soywiz.korma.geom.ScaleMode
 import com.soywiz.korma.geom.Size
 import com.soywiz.korma.geom.SizeInt
 
@@ -16,6 +17,7 @@ object GameModule : Module() {
     override val windowSize: SizeInt = SizeInt(Size(1920, 1080))
     override val size: SizeInt = SizeInt(Size(384, 216))
     override val bgcolor = Colors["#2b2b2b"]
+    override val scaleMode = ScaleMode.COVER
 
     override suspend fun AsyncInjector.configure() {
         val assets = Assets().apply { init() }
