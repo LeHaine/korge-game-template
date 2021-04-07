5221a1a4ae6fc3e4e38472f108639cfe628cb9ff
LeHaine <colt.daily@gmail.com>
2021-04-07 13:18:26 -0400

    add Affect type
    update Entity to handle Affect updates

--- src/commonMain/kotlin/com/lehaine/game/Entity.kt
+++ src/commonMain/kotlin/com/lehaine/game/Entity.kt
@@ -2,6 +2,7 @@ package com.lehaine.game
 
 import com.lehaine.kiwi.component.BaseGameEntity
 import com.lehaine.game.component.GenericGameLevelComponent
+import com.soywiz.klock.TimeSpan
 import com.soywiz.korge.view.Container
 
 /**
@@ -23,4 +24,46 @@ import com.soywiz.korge.view.Container
 open class Entity(override val level: GenericGameLevelComponent<LevelMark>, container: Container) :
     BaseGameEntity(level, container) {
     val fx get() = level.fx
+
+    private val affects = hashMapOf<Affect, TimeSpan>()
+
+    override fun update(dt: TimeSpan) {
+        super.update(dt)
+        updateAffects(dt)
+    }
+
+    fun hasAffect(affect: Affect) = affects.containsKey(affect)
+
+    fun addAffect(affect: Affect, duration: TimeSpan, addToCurrentDuration: Boolean = true) {
+        if (affects.containsKey(affect)) {
+            if (addToCurrentDuration) {
+                affects[affect] = affects[affect]?.plus(duration) ?: duration
+                return
+            }
+        }
+        affects[affect] = duration
+        onAffectStart(affect)
+    }
+
+    fun removeAffect(affect: Affect) {
+        affects.remove(affect)
+        onAffectEnd(affect)
+    }
+
+    open fun onAffectStart(affect: Affect) {}
+    open fun onAffectUpdate(affect: Affect) {}
+    open fun onAffectEnd(affect: Affect) {}
+
+    private fun updateAffects(dt: TimeSpan) {
+        affects.keys.forEach {
+            var remainingTime = affects[it] ?: TimeSpan.ZERO
+            remainingTime -= dt
+            if (remainingTime <= TimeSpan.ZERO) {
+                removeAffect(it)
+            } else {
+                affects[it] = remainingTime
+                onAffectUpdate(it)
+            }
+        }
+    }
 }
