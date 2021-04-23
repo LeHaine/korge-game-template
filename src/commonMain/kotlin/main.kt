5d0b94d3dc342579aa6c4e9a9a786abb24c2050e
LeHaine <colt.daily@gmail.com>
2021-04-23 12:59:02 -0400

    update default scale mode, window size, and virtual size

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -4,9 +4,9 @@ import com.lehaine.game.World
 import com.soywiz.korge.Korge
 import com.soywiz.korge.scene.Module
 import com.soywiz.korge.view.views
+import com.soywiz.korgw.GameWindow
 import com.soywiz.korim.color.Colors
 import com.soywiz.korinject.AsyncInjector
-import com.soywiz.korma.geom.ScaleMode
 import com.soywiz.korma.geom.Size
 import com.soywiz.korma.geom.SizeInt
 
@@ -15,10 +15,9 @@ suspend fun main() = Korge(Korge.Config(module = GameModule))
 object GameModule : Module() {
     override val mainScene = LevelScene::class
 
-    override val windowSize: SizeInt = SizeInt(Size(1920, 1080))
-    override val size: SizeInt = SizeInt(Size(384, 216))
+    override val windowSize: SizeInt = SizeInt(Size(480 * 3, 270 * 3))
+    override val size: SizeInt = SizeInt(Size(480, 270))
     override val bgcolor = Colors["#2b2b2b"]
-    override val scaleMode = ScaleMode.COVER
 
     override suspend fun AsyncInjector.configure() {
         Assets.init(views())
