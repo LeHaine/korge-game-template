39433339c3f8ae43e7fd162ddb6afe8bb36742a2
LeHaine <colt.daily@gmail.com>
2021-05-17 09:47:14 -0400

    update to korge 2.1.1.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,23 @@
 pluginManagement {
     repositories {
         mavenLocal()
-        maven("https://jitpack.io")
-        maven("https://dl.bintray.com/korlibs/korlibs")
-        maven("https://plugins.gradle.org/m2/")
         mavenCentral()
         google()
+        maven("https://plugins.gradle.org/m2/")
     }
 
     val korgePluginVersion: String by settings
 
     plugins {
         id("com.soywiz.korge") version korgePluginVersion
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.soywiz") {
+                useModule("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+            }
+        }
     }
 }
 
